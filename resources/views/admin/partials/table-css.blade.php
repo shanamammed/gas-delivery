47f92e024988eb1f9321bfbf0bcbcb9efd8470f4
shanamammed <shanamammed96@gamil.com>
2022-10-22 02:10:09 +0530

    table scc file

---
+++ resources/views/admin/partials/table-css.blade.php
@@ -0,0 +1,2 @@
+<link href="{{ asset('/plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('/plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
