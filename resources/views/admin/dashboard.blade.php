d32480ae8d936e83ff0a413fc9da080151544e4b
shanamammed <shanamammed96@gamil.com>
2022-10-22 22:28:37 +0530

    dashbord view page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -9,7 +9,7 @@
                     <div class="page-title-box">
                         <h4 class="page-title float-left">Dashboard</h4>
                         <ol class="breadcrumb float-right">
-                            <li class="breadcrumb-item"><a href="#">Hungerline</a></li>
+                            <li class="breadcrumb-item"><a href="#">Gas Delivery</a></li>
                             <li class="breadcrumb-item active">Dashboard</li>
                         </ol>
                         <div class="clearfix"></div>
@@ -18,49 +18,56 @@
             </div>
             <!-- end row -->
             <div class="row">
-                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <i class="fa fa-shopping-cart float-right"></i>
-                            <!-- <h6 class="text-muted text-uppercase mb-3"> Orders<br> (<?php echo date('M Y'); ?> )</h6>
-                             <h4 class="mb-3" data-plugin="counterup"></h4> --> 
+                            <h6 class="text-muted text-uppercase mb-3"> Today's Orders<br> (<?php echo date('d M Y'); ?> )</h6>
+                             <h4 class="mb-3" data-plugin="counterup">{{ $orders_today }}</h4> 
                         </div>
                     </a>
                 </div>
                 
-                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <i class="fa fa-shopping-cart float-right"></i>
-                           <!--  <h6 class="text-muted text-uppercase mb-3"> Today's Orders<br> (<?php echo date('d M Y'); ?> )</h6>
-                             <h4 class="mb-3" data-plugin="counterup"></h4>  -->
+                            <h6 class="text-muted text-uppercase mb-3"> Weekly Orders<br> (<?=date('w')?>)</h6>
+                             <h4 class="mb-3" data-plugin="counterup">{{ $orders_weekly }}</h4> 
                         </div>
                     </a>
                 </div>
                 
-                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <i class="fa fa-shopping-cart float-right"></i>
-                           <!--  <h6 class="text-muted text-uppercase mb-3"> Cancelled Orders Todat<br> (<?php echo date('d M Y'); ?> )</h6>
-                             <h4 class="mb-3" data-plugin="counterup"></h4>  -->
+                            <h6 class="text-muted text-uppercase mb-3"> Monthly Orders<br> (<?php echo date('M Y'); ?> )</h6>
+                             <h4 class="mb-3" data-plugin="counterup">{{ $orders_monthly }}</h4> 
                         </div>
                     </a>
                 </div>
-
-                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+                
+                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-4">
                         <div class="card-box tilebox-one">
-                            <i class="fa fa-coffee float-right"></i>
-                            <h6 class="text-muted text-uppercase mb-3">Active Restaurants</h6>
-                            <!-- <h4 class="mb-3" data-plugin="counterup"></h4> -->
+                            <i class="fa fa-user float-right"></i>
+                            <h6 class="text-muted text-uppercase mb-3">Users</h6>
+                            <h4 class="mb-3" data-plugin="counterup">{{ $users }}</h4>
                         </div>
                     </a>    
                 </div>
 
-              
+                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-4">
+                        <div class="card-box tilebox-one">
+                            <i class="fa fa-flag float-right"></i>
+                            <h6 class="text-muted text-uppercase mb-3">Services</h6>
+                            <h4 class="mb-3" data-plugin="counterup">{{ $services }}</h4>
+                        </div>
+                    </a>    
+                </div>
 
-                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+                <div class="col-xs-12 col-md-6 col-lg-6 col-xl-4">
                         <div class="card-box tilebox-one">
                             <i class="fa fa-users float-right"></i>
-                            <h6 class="text-muted text-uppercase mb-3">Total Customers</h6>
-                            <!-- <h4 class="mb-3" data-plugin="counterup"></h4> -->
+                            <h6 class="text-muted text-uppercase mb-3">Drivers</h6>
+                            <h4 class="mb-3" data-plugin="counterup">{{ $drivers }}</h4>
                         </div>
                     </a>    
                 </div>
@@ -68,35 +75,9 @@
             <!-- end row -->
             <div class="row">
         
+         
 
-                <div class="col-lg-4">
-                    <div class="card-box">
-                        <h4 class="header-title mb-4">New Restaurants</h4>
-                        <div class="inbox-widget slimscroll" style="max-height: 670px;">
-                        
-                        </div>
-                    </div>
-                </div>
-
-                
-                <div class="col-lg-4">
-                    <div class="card-box">
-                        <h4 class="header-title mb-4">New Orders</h4>
-                        <div class="inbox-widget slimscroll" style="max-height: 670px;">
-                          
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-lg-4">
-                    <div class="card-box">
-                        <h4 class="header-title mb-4">New Customers</h4>
-                        <div class="inbox-widget slimscroll" style="max-height: 670px;">
-                          
-                        </div>
-                    </div>
-                </div>
-
+             
             </div>
         </div> <!-- container -->
     </div> <!-- content -->
